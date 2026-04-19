6c1a30ded27d63b2a2edd4db883c1bb72f69d1a6
alvarez.894 <alvarez.894@gmail.com>
2026-04-19 05:38:35 +0000

    fix: Centra la imagen de fondo para mantener el foco en la nebulosa

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,19 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Footer from "@/app/components/Footer"; // Importamos el componente Footer
+import Footer from "@/app/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Santuario de Amor - Guía Espiritual y Sanación Emocional",
-  description: "Encuentra la paz interior y el amor verdadero a través de nuestros servicios espirituales. Amarres, limpiezas energéticas, y más. Consulta gratuita.",
+  description: "Encuentra la paz interior y el amor verdadero a través de nuestros services espirituales. Amarres, limpiezas energéticas, y más. Consulta gratuita.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body className={`${inter.className} bg-black bg-cover bg-fixed bg-[url('/background.jpg')]`}>
+      <body className={`${inter.className} bg-black bg-cover bg-center bg-fixed bg-[url('/background.jpg')]`}>
         {children}
         <Footer />
       </body>
